e2320bc69a0438f471e2b4ab5fa8506b9b260e68
BogdanowOS <47810075+BogdanowOS@users.noreply.github.com>
2023-03-15 18:30:59 +0200

    Add files via upload

--- MyArrayList.java
+++ MyArrayList.java
@@ -1,16 +1,16 @@
-public class MyArrayList {
+public class MyArrayList<T> {
 
-    private Object[] array;
+    private T[] array;
     private int size;
 
     public MyArrayList() {
-        array = new Object[10];
+        array = (T[]) new Object[10];
         size = 0;
     }
 
-    public void add(Object value) {
+    public void add(T value) {
         if (size == array.length) {
-            Object[] newArray = new Object[array.length * 2];
+            T[] newArray = (T[]) new Object[array.length * 2];
             System.arraycopy(array, 0, newArray, 0, array.length);
             array = newArray;
         }
@@ -29,7 +29,7 @@ public class MyArrayList {
     }
 
     public void clear() {
-        array = new Object[10];
+        array = (T[]) new Object[10];
         size = 0;
     }
 
@@ -37,7 +37,7 @@ public class MyArrayList {
         return size;
     }
 
-    public Object get(int index) {
+    public T get(int index) {
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException();
         }
